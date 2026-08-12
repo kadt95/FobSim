672363625550b9599c853c1e86f4813f9e1fd79e
kadt95 <232221269+kadt95@users.noreply.github.com>
2026-08-12 14:39:19 +0200

    fix potential division by zero

--- end_user.py
+++ end_user.py
@@ -41,7 +41,7 @@ class User:
         operations = ['+', '-', '*', '/']
         for task_number in range(num_of_task_per_user):
             operation = random.choice(operations)
-            random_computational_task = str(randrange(1000000)) + operation + str(randrange(1000000))
+            random_computational_task = str(randrange(1000000)) + operation + str(randrange(1,1000000))
             self.tasks.append([self.addressParent, self.addressSelf, random_computational_task, blockchain_function])
 
     def __apply_third_functionality(self, num_of_task_per_user, list_of_end_users, blockchain_function):
